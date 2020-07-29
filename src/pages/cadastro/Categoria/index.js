09bb13d1b4eae746fa98b92d9fce083005be7ccf
GiselyGarcia <gisely.gsouzaa@gmail.com>
2020-07-29 20:47:43 -0300

    aula 3

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
+
 function CadastroCategoria() {
-    return(
-      <PageDefault>
-        <h1> cadastro de categoria</h1>
-        <Link to="/">
-          Ir para home
-        </Link>
-      </PageDefault>
-    )
-  }
-  export default CadastroCategoria;
+  const [categorias, setCategorias] = useState(['teste']);
+  const [nomeDaCategoria, setNomeDaCategoria] = useState('Desenhos');
+    return (
+    <PageDefault>
+      <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
+
+      <form onSubmit={function handleSubmit(infosDoEvento){
+        infosDoEvento.preventDefault();
+        setCategorias([
+          ...categorias,
+          nomeDaCategoria
+        ]);
+      }}>
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+            value={nomeDaCategoria}
+            onChange={function funcaoHandler(infosDoEvento) {
+              setNomeDaCategoria(infosDoEvento.target.value);
+            }}
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return (
+            <li key ={`${categoria}${indice}`}>
+              {categoria}
+            </li>
+          )
+        })}
+      </ul>
+
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
+}
+
+export default CadastroCategoria;
